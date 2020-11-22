7f76e619f38efd4f40e32b942b8dd55b9ec21e5d
SPARKS <umberto.zollo@gmail.com>
2020-11-22 17:34:32 +0100

    Modfiche

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,29 @@
-$('.carousel').carousel({
-  interval: 2000
-})
+$(document).ready(function () {
+//
+//  $('.carousel').carousel({
+//    interval: 1000
+//  });
+//
+  $('.fa-angle-double-left').addClass('fa-3x');
+  $('.fa-angle-double-right').addClass('fa-3x');
+  
+  $('.fa-angle-double-left')
+    .mouseenter(function () {
+      $('.fa-angle-double-left').toggleClass('fa-3x')
+      $('.fa-angle-double-left').addClass('fa-5x');
+    })
+    .mouseout(function () {
+      $('.fa-angle-double-left').toggleClass('fa-5x');
+      $('.fa-angle-double-left').addClass('fa-3x');
+    })
+    
+  $('.fa-angle-double-right')
+    .mouseenter(function () {
+      $('.fa-angle-double-right').toggleClass('fa-3x')
+      $('.fa-angle-double-right').addClass('fa-5x');
+    })
+    .mouseout(function () {
+      $('.fa-angle-double-right').toggleClass('fa-5x');
+      $('.fa-angle-double-right').addClass('fa-3x');
+    })
+});
