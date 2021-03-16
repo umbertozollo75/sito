67d94ffb056ceca7e71fe7a0e415833bbf005d4a
SPARKS <umberto.zollo@gmail.com>
2021-03-16 23:37:32 +0100

    Modfiche

---
+++ index1.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang='en-US'>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title>Umberto Zollo Photography</title>
+    <!-- JQuery, Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="./bootstrap-4.5.0-dist/css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css">
+    
+    <link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat+Alternates:400%7CRoboto:300,400">
+    <!-- custom css-->
+    <link type="text/css" rel="stylesheet" href="./css/style1.css" />
+  </head>
+  <body class="" style="">
+    <!-- Page Header-->
+    <header class="page-head header-full-height">
+      <!-- RD Navbar-->
+      <div class="rd-navbar-wrap" style="height: 0px;">
+        <nav class="navbar navbar-expand-lg">
+          <div class="rd-navbar-inner">
+
+            <div class="rd-navbar-panel rd-navbar-panel-left">
+              <!-- RD Navbar Brand-->
+              <a href="index.html" class="">
+                  <span class="brand-name">Umberto Zollo</span>
+              </a>
+              <p>PHOTOGRAPHY</p>
+            </div>
+            <div class="rd-navbar-panel rd-navbar-panel-center">
+              <?php include 'social1.php'; ?>
+            </div>
+            <div class="rd-navbar-panel rd-navbar-panel-right">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav ml-auto navbar-custom">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"> Home</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Portfolio</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a class="dropdown-item" href="#">2021</a>
+                      <a class="dropdown-item" href="#">2020</a>
+                      <a class="dropdown-item" href="#">2019</a>
+                    </div>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="news.php"> News</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contatti.php"> Contatti</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <!-- Swiper-->
+      <div class="section-head">
+        <div class="section-container">
+          <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel" data-interval="5000">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="./image/home-slider01.jpg" alt="">
+                <div class="carousel-caption">
+                  <h3 class="h3-responsive">GIORNATA GLAMOUR</h3>
+                  <p>...</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./image/home-slider02.jpg" alt="">
+                <div class="carousel-caption">
+                  <h3 class="h3-responsive">GIORNATA GLAMOUR</h3>
+                  <p>...</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./image/home-slider03.jpg" alt="">
+                <div class="carousel-caption">
+                  <h3 class="h3-responsive">GIRO DELL'APPENNINO 2020</h3>
+                  <p>Genova</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./image/home-slider04.jpg" alt="">
+                <div class="carousel-caption">
+                  <h3 class="h3-responsive">GIRO DEL PIEMONTE 2019</h3>
+                  <p>...</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./image/home-slider05.jpg" alt="">
+                <div class="carousel-caption">
+                  <h3 class="h3-responsive">GIRO DEL PIEMONTE 2019</h3>
+                  <p>Oropa (BI)</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./image/home-slider00.jpg" alt="">
+                <div class="carousel-caption">
+                  <h3 class="h3-responsive"></h3>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <!--<span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+              <span><i class="fa fa-angle-double-left"></i></span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <!--<span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+              <span><i class="fa fa-angle-double-right"></i></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <?php include 'footer.php'; ?>
+
+    <!-- JS -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./js/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="./bootstrap-4.5.0-dist/js/bootstrap.min.js"></script>
+    <!-- custom js -->   
+    <script type="text/javascript" src="./js/custom.js"></script>
+  </div>
+</body>
+</html>
---
+++ social1.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<ul class="list-inline">
+  <li class="list-inline-item">
+    <a href="https://www.facebook.com/umberto.zollo" class="icon-default icon-xs icoFacebook" target="_blank"><i class="fa fa-facebook"></i></button>
+  </li>
+  <li class="list-inline-item">
+    <a href="https://twitter.com/UmbertoZollo" class="icon-default icon-xs icoTwitter" target="_blank"><i class="fa fa-twitter"></i></a>
+  </li>
+  <li class="list-inline-item">
+    <a href="https://www.instagram.com/umbertozollo" class="icon-default icon-xs icoInstagram" target="_blank"><i class="fa fa-instagram"></i></a>
+  </li>
+  <li class="list-inline-item">
+    <a href="https://500px.com/p/umbertozollo75" class="icon-default icon-xs ico500px" target="_blank"> <i class="fa fa-500px"></i></a>
+  </li>
+</ul>
